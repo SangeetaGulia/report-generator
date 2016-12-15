385719dbbd763e63546c0f0a119722f71ca93b96
SangeetaGulia <sangeeta.gulia@knoldus.in>
2016-12-15 14:05:30 +0530

    Added initial layout

--- src/main/java/ReportGenerator.java
+++ src/main/java/ReportGenerator.java
@@ -7,12 +7,12 @@ public class ReportGenerator {
         try {
             File file = new File("index.html");
             OutputStream outputStream = new FileOutputStream(file);
-            System.out.println("Output Stream" + outputStream);
             PrintWriter writer = new PrintWriter(outputStream);
 
             String indexContent = createLayout();
             writer.print(indexContent);
 
+            System.out.println("Generated index.html successfully");
             writer.close();
             outputStream.close();
         }
@@ -30,11 +30,6 @@ public class ReportGenerator {
                "<title> Performance Report </title>\n" +
                "\n";
 
-
-        String addScript = "<script>\n" +
-                "\n" +
-                "</script>\n";
-
         String startBody = "\n" +
                 "</head>\n" +
                 "\n" +
@@ -46,31 +41,59 @@ public class ReportGenerator {
                 "\n" +
                 "</html>";
 
-        String layout = htmlStart + addScript + startBody + addLeftSideBar() + addRightSidebar() + closeBodyAndHtml;
+
+
+        String layout = htmlStart + addScript() + startBody + startContainer() + addLeftSideBar() + addRightSidebar() + closeContainer() + closeBodyAndHtml;
 
         return layout;
 
     }
 
+    String addScript(){
+        return "    <meta charset=\"utf-8\">\n" +
+                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" +
+                "    <link rel=\"stylesheet\" href=\"src/main/resources/bootstrap-3.3.6-dist/css/bootstrap.min.css\">\n" +
+                "    <script src=\"src/main/resources/jquery/jquery-2.2.0.min.js\"></script>\n" +
+                "    <script src=\"src/main/resources/bootstrap-3.3.6-dist/js/bootstrap.min.js\"></script>\n" +
+                "\n" +
+                "    <script src=\"src/main/resources/custom/css/leftSidebar.css\"></script>\n" +
+                "    <script src=\"src/main/resources/custom/css/rightSidebar.css\"></script>";
+    }
+
+    String startContainer() {
+        return "\n" +
+                "<div class=\"container\">\n" +
+                "<div class=\"row\">\n" +
+                "\n" +
+                "    ";
+    }
+
+    String closeContainer() {
+        return "\n" +
+                "</div>\n" +
+                "</div>\n" +
+                "\n";
+    }
+
     String addLeftSideBar() {
-      return  "<div id=\"leftSidebar\">\n" +
-              "leftSidebar\n" +
-              "</div>";
+      return  "\n" +
+              "    <div class=\"col-sm-4\">\n" +
+              "    <div id=\"leftSidebar\">\n" +
+              "        leftSidebar\n" +
+              "    </div>\n" +
+              "    </div>\n" +
+              "    \n" +
+              "   ";
     }
 
     String addRightSidebar(){
         return "\n" +
-                "\n" +
-                "<div id = \"rightSidebar\">\n" +
-                "rightsidebar\n" +
-                "</div>\n";
+                "    <div class=\"col-sm-8\">\n" +
+                "    <div id=\"rightSidebar\">\n" +
+                "        rightSidebar\n" +
+                "    </div>\n" +
+                "    </div>\n" +
+                "    ";
     }
-
-    void createPerformanceSummaryReport(){
-
-    }
-
-
-
 
 }
